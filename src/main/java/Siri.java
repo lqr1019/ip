2b1b9e1dbb8175bcd8b0e315eaf9aee87d4be997
lqr1019 <liqingrui1019@foxmail.com>
2025-08-20 14:38:20 +0800

    rename chatbox and change logo

---
+++ src/main/java/Siri.java
@@ -0,0 +1,16 @@
+public class Siri {
+    public static void main(String[] args) {
+        //Generate from https://patorjk.com/software/taag/
+        String siriLogo = "   _____ _      _ \n" +
+                "  / ____(_)    (_)\n" +
+                " | (___  _ _ __ _ \n" +
+                "  \\___ \\| | '__| |\n" +
+                "  ____) | | |  | |\n" +
+                " |_____/|_|_|  |_|\n" +
+                "                  \n" +
+                "                  ";
+
+        System.out.print("Hello from\n" + siriLogo);
+
+    }
+}
