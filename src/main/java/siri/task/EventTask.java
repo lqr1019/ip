d125bce43487eb06aab2770122bb330eae233a7f
lqr1019 <liqingrui1019@foxmail.com>
2025-09-20 15:43:55 +0800

    Add JavaDoc for EventTask

--- src/main/java/siri/task/EventTask.java
+++ src/main/java/siri/task/EventTask.java
@@ -4,6 +4,9 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
+/**
+ * Class for EventTask
+ */
 public class EventTask extends Task {
     private String from;
     private String to;
